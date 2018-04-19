0271164d0742cbd71c69aad87434db087e08e24d
tkobialka <tkobialka@gmail.com>
2018-04-19 18:38:28 +0100

    Added working express backend

--- server.js
+++ server.js
@@ -1,111 +1,46 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const Web3 = require('web3');
-const truffle_connect = require('./connection/app.js');
-//const truffle_connect = require('./public_static/js/app.js');
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
 const bodyParser = require('body-parser');
+
+
+var fs = require("fs"),
+    json;
+
+
+json = getConfig('narrativeChainy.json');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
-app.use(bodyParser.json());
+app.use('/', express.static('src'));
 
 
-app.use('/', express.static('public_static'));
-
-/*
- if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
-    truffle_connect.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-  }
-*/
-
-// Should happen by default.
-app.get('/getAccounts', (req, res) => {
-
-  console.log("**** GET /getAccounts START ****");
-  //console.dir(req);
-  console.dir(this.web3);
-
-  truffle_connect.start(function (answer) {
-   // res.send(answer);
-  });
-
-  console.log(" ****** NOW LOAD THE NARRATIVE ******");
-  var narrativelength;
-  
-  // refresh narrative should return an array.. I guess.
-  truffle_connect.refreshNarrative(function (answer) {
-  
-    console.log(" +++++++ answer: " + answer);
-    console.dir(answer);
-   // narrativelength = answer;
-   res.send(answer);
-        // This doesn't work. Cannot have individual items sent via callback. Must be an array.
-        /*
-
-         for (var i = 0; i < narrativelength; i++) {
-           //console.log("in loop " + i);
-            truffle_connect.retrieveStoryItem(i, function (answer) {
-                //narrativelength = answer;
-                //res.send(answer);
-                console.log(" i = " + i + "answer" + answer);
-                res.send(answer);
-            });
-         }
-         */
-  })
-  //console.log("narrativelength: " + narrativelength);
+app.get('/jsoncontent',function(req,res){
+  res.send(json);
 });
 
-app.post('/contribute', (req, res) => {
-  //alert("SENDCOIN");
-  console.log("**** POST contributeText ****");
-  console.log(req.body);
 
-  let narrative = req.body.narrative;
- /* let amount = req.body.amount;
-  let sender = req.body.sender;
-  let receiver = req.body.receiver;
-  */
+function readJsonFileSync(filepath, encoding){
 
-  truffle_connect.contributeText(narrative, (answer) =>{
-    console.log("contribute TEXT ANSWER!!");
-    res.send(answer);
-  });
- /* truffle_connect.sendCoin(amount, sender, receiver, (balance) => {
-    res.send(balance);
-  });
-  */
-});
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
 
-app.listen(port, () => {
+function getConfig(file){
 
-  // Maybe contract should be found AND loaded here?! When server starts!! IDK..
-  console.log("IN APP.LISTEN");
+    var filepath = __dirname + '/build/contracts/' + file;
+    console.dir(filepath);
+    console.log("filepath: " + filepath);
+    return readJsonFileSync(filepath);
+}
 
-  // Move this to clientside..? on pg 'load'
 
-/*
-  if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
-    truffle_connect.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:7545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-  }
-  */
-  
-  console.log("Express Listening at http://localhost:" + port);
+app.listen(3007);
 
-});
+console.log("Running at Port 3007");
--- truffle.js
+++ truffle.js
@@ -1,19 +1,37 @@
-// Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// Just for TEST. Hide for production.
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 7545,
-      network_id: '*'
-    },
-    ropsten: {
+	  // See <http://truffleframework.com/docs/advanced/configuration>
+	  // to customize your Truffle configuration!
+	  networks: {
+	  	development: {
+	  		host: "localhost",
+	  		port: 7545,
+	   		network_id: "*" // Match any network id
+	},
+	ropsten: {
 		provider: function() {
 			return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/Ax5n010X0yJC4cLit0ra")
 		},
 		network_id: 3, // Official ropsten network id
-		gas: 4712388
-	}   
-  }
+		//gas: 4712388
+    //from: "0xf17f52151EbEF6C7334FAD080c5704D77216b732",
+    //gas:4612388
+    gas: 4000000
+	},
+  kovan: {
+    provider: function() {
+      //return new HDWalletProvider(mnemonic, "https://kovan.infura.io/Ax5n010X0yJC4cLit0ra")
+      return new HDWalletProvider(mnemonic, " https://kovan.infura.io/AaVyIWCyFSiYHLjWkGQJ")
+    },
+    network_id: 42, // Official ropsten network id
+    //gas: 4712388
+    //from: "0xf17f52151EbEF6C7334FAD080c5704D77216b732",
+    //gas:4612388
+    gas: 4000000
+  }      
 }
+};
